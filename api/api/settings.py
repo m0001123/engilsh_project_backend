ba5a24faa594fbc7320305bc2299b536003e9bd1
Hsinwei Chen <7691822+edwinyoo44@users.noreply.github.com>
2022-04-11 02:59:35 +0800

    Update settings.py

--- api/api/settings.py
+++ api/api/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'alicsnet_api',
+        'USER': 'alicsnet_api',
+        'PASSWORD': 'rAgu9H_QKX.C*DId',
+        'HOST': 'database.cluster-c07wabporzi8.us-west-1.rds.amazonaws.com',
+        'PORT': '3306'
     }
 }
 

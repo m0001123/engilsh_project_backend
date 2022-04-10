c3381efbc4b4b71b4a408c0dac540b7537047f15
Hsinwei Chen <7691822+edwinyoo44@users.noreply.github.com>
2022-04-11 02:28:18 +0800

    Update pull_and_restart.sh

--- pull_and_restart.sh
+++ pull_and_restart.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-git fetch origin
-git pull --force origin master
+#先取回遠端數據庫的最新歷史紀錄
+git fetch --all 
+#然後放棄目前所有檔案與 commit，還原成遠端版本
+git reset --hard origin/master
+#最後重新拉回來
+git pull origin master
 
 pipenv run uwsgi --reload /tmp/api-master.pid
 

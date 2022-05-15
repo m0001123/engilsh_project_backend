c695f23d810194f2dfc3046ceb9d98065c3f67ab
TMQPR <75326607+TMQPR@users.noreply.github.com>
2022-05-15 18:24:19 +0800

    add_sentence_segmentation

--- api/app/code/sentenceController.py
+++ api/app/code/sentenceController.py
@@ -1,32 +1,35 @@
 import pymysql
 from django.http import JsonResponse
 import json
-from eng_to_ipa import eng_to_ipa 
+import spacy
+from eng_to_ipa import eng_to_ipa
 from similar_text import similar_text
 from random import choice, sample
 import re
 
 from app.code import appAPIUtils
 
+
 def getSentenceTopicData():
     try:
         data = appAPIUtils.getSentenceTopicData()
-       
+
         # 初始化回傳值
         response = {
-            'apiStatus' : 'success',
-			'apiMessage' : 'success',
-			'data' : data,
+            'apiStatus': 'success',
+            'apiMessage': 'success',
+            'data': data,
         }
     except Exception as e:
         # 初始化回傳值
         response = {
-            'apiStatus' : 'error',
-			'apiMessage' : str(e),
+            'apiStatus': 'error',
+            'apiMessage': str(e),
         }
 
     # 將回傳值回傳
     return JsonResponse(response)
+
 
 def getSentences(request):
     try:
@@ -42,55 +45,57 @@ def getSentences(request):
         dataLimit = request.POST.get('dataLimit', '')
 
         # 判斷資料
-        #if (sentenceLevel == ''):    
+        # if (sentenceLevel == ''):
         #    raise Exception('缺少必填參數 sentenceLevel')
-   
+
         # 整理從資料庫得到的資料
 
-        dataArray = appAPIUtils.getSentencesData(sentenceLevel = sentenceLevel, sentenceTopic = sentenceTopic, sentenceClass = sentenceClass, aboutWord = aboutWord, sentenceMinLength = sentenceMinLength, sentenceMaxLength = sentenceMaxLength, sentenceRanking = sentenceRanking, sentenceRankingLocking = sentenceRankingLocking, dataLimit = dataLimit)
+        dataArray = appAPIUtils.getSentencesData(sentenceLevel=sentenceLevel, sentenceTopic=sentenceTopic, sentenceClass=sentenceClass, aboutWord=aboutWord, sentenceMinLength=sentenceMinLength,
+                                                 sentenceMaxLength=sentenceMaxLength, sentenceRanking=sentenceRanking, sentenceRankingLocking=sentenceRankingLocking, dataLimit=dataLimit)
         # 初始化回傳值
         response = {
-            'apiStatus' : 'success',
-			'apiMessage' : 'success',
-			'data' : dataArray,
+            'apiStatus': 'success',
+            'apiMessage': 'success',
+            'data': dataArray,
         }
     except Exception as e:
         # 初始化回傳值
         response = {
-            'apiStatus' : 'error',
-			'apiMessage' : str(e),
+            'apiStatus': 'error',
+            'apiMessage': str(e),
         }
 
     # 將回傳值回傳
     return JsonResponse(response)
 
-    
+
 def getSentencesByID(request):
     try:
         # 蒐集資料
         sentencesID = request.POST.get('sentencesID', '')
 
         # 判斷資料
-        if (sentencesID == ''):    
+        if (sentencesID == ''):
             raise Exception('缺少必填參數 sentencesID')
-   
+
         # 整理從資料庫得到的資料
-        dataArray = appAPIUtils.getSentencesData(sentencesID = sentencesID)
+        dataArray = appAPIUtils.getSentencesData(sentencesID=sentencesID)
         # 初始化回傳值
         response = {
-            'apiStatus' : 'success',
-			'apiMessage' : 'success',
-			'data' : dataArray,
+            'apiStatus': 'success',
+            'apiMessage': 'success',
+            'data': dataArray,
         }
     except Exception as e:
         # 初始化回傳值
         response = {
-            'apiStatus' : 'error',
-			'apiMessage' : str(e),
+            'apiStatus': 'error',
+            'apiMessage': str(e),
         }
 
     # 將回傳值回傳
     return JsonResponse(response)
+
 
 def getPhoneticExercisesSentencesByWordSet(request):
     try:
@@ -99,35 +104,37 @@ def getPhoneticExercisesSentencesByWordSet(request):
         learningPhase = request.POST.get('learningPhase', '')
 
         # 判斷資料
-        if (learningClassification == ''):    
+        if (learningClassification == ''):
             raise Exception('缺少必填參數 learningClassification')
-        if (learningPhase == ''):    
+        if (learningPhase == ''):
             raise Exception('缺少必填參數 learningPhase')
-   
+
         # 整理從資料庫得到的資料
-        wordData = appAPIUtils.getWordLearningData(learningClassification = learningClassification, learningPhase = learningPhase)
+        wordData = appAPIUtils.getWordLearningData(
+            learningClassification=learningClassification, learningPhase=learningPhase)
 
         sentencesData = []
         for word in wordData:
-            dataArray = appAPIUtils.getSentencesData(sentenceRankingLocking = word['wordRanking'], dataLimit = '3')
+            dataArray = appAPIUtils.getSentencesData(
+                sentenceRankingLocking=word['wordRanking'], dataLimit='3')
             sentencesData = sentencesData + dataArray
-        
-        
+
         # 初始化回傳值
         response = {
-            'apiStatus' : 'success',
-			'apiMessage' : 'success',
-			'data' : sentencesData,
+            'apiStatus': 'success',
+            'apiMessage': 'success',
+            'data': sentencesData,
         }
     except Exception as e:
         # 初始化回傳值
         response = {
-            'apiStatus' : 'error',
-			'apiMessage' : str(e),
+            'apiStatus': 'error',
+            'apiMessage': str(e),
         }
 
     # 將回傳值回傳
     return JsonResponse(response)
+
 
 def checkSentences(request):
     try:
@@ -144,13 +151,12 @@ def checkSentences(request):
         if (correctCombo == ''):
             correctCombo = 0
 
-
         # 獲取IPA文字(無標點符號)
         questionIPAText = eng_to_ipa.convert(questionText, False, False)
         answerIPAText = eng_to_ipa.convert(answerText, False, False)
 
         # 處理文字 移除標點符號
-        replaceSymbol= [',', '.', '!', '?']
+        replaceSymbol = [',', '.', '!', '?']
         for i in range(len(replaceSymbol)):
             questionText = questionText.replace(replaceSymbol[i], ' ')
             answerText = answerText.replace(replaceSymbol[i], ' ')
@@ -165,7 +171,8 @@ def checkSentences(request):
         ipaTextSimilarity = similar_text(questionIPAText, answerIPAText)
 
         # 根據IPA相似度給予評價
-        scoreComment = appAPIUtils.getScoreComment(ipaTextSimilarity, correctCombo)
+        scoreComment = appAPIUtils.getScoreComment(
+            ipaTextSimilarity, correctCombo)
 
         # 獲取有問題的單字陣列(用發音判斷)
         questionErrorWordArray = []
@@ -192,62 +199,66 @@ def checkSentences(request):
         for errorWord in questionErrorWordArray:
             ipaAboutArray = {}
             rhymesWordArray = eng_to_ipa.get_rhymes(errorWord)
-            for ipaAboutWord in sample(rhymesWordArray, min(len(rhymesWordArray),3)):
-                pronunciationData = appAPIUtils.getWordPronunciationData(ipaAboutWord)
+            for ipaAboutWord in sample(rhymesWordArray, min(len(rhymesWordArray), 3)):
+                pronunciationData = appAPIUtils.getWordPronunciationData(
+                    ipaAboutWord)
                 ipaAboutArray.update({ipaAboutWord: pronunciationData})
                 pass
             pronunciationData = appAPIUtils.getWordPronunciationData(errorWord)
-            pronunciationData.update({'ipaAbout' : ipaAboutArray})
+            pronunciationData.update({'ipaAbout': ipaAboutArray})
             questionError.update({errorWord: pronunciationData})
             pass
-        
+
         # 獲取回答中錯誤相關的資料
         answerError = {}
         for errorWord in answerErrorWordArray:
             ipaAboutArray = {}
             rhymesWordArray = eng_to_ipa.get_rhymes(errorWord)
-            for ipaAboutWord in sample(rhymesWordArray, min(len(rhymesWordArray),3)):
-                pronunciationData = appAPIUtils.getWordPronunciationData(ipaAboutWord)
+            for ipaAboutWord in sample(rhymesWordArray, min(len(rhymesWordArray), 3)):
+                pronunciationData = appAPIUtils.getWordPronunciationData(
+                    ipaAboutWord)
                 ipaAboutArray.update({ipaAboutWord: pronunciationData})
                 pass
             pronunciationData = appAPIUtils.getWordPronunciationData(errorWord)
-            pronunciationData.update({'ipaAbout' : ipaAboutArray})
+            pronunciationData.update({'ipaAbout': ipaAboutArray})
             answerError.update({errorWord: pronunciationData})
             pass
 
         # IPA發音檢驗
-        checkPronunciation = appAPIUtils.checkPronunciation(questionIPAText, answerIPAText)
+        checkPronunciation = appAPIUtils.checkPronunciation(
+            questionIPAText, answerIPAText)
 
         # 整理資料
         dataArray = {
-            'questionText' : questionText,
-            'questionIPAText' : questionIPAText,
-            'answerText' : answerText,
-            'answerIPAText' : answerIPAText,
-            'textSimilarity' : textSimilarity,
-            'ipaTextSimilarity' : ipaTextSimilarity,
-            'scoreComment' : scoreComment,
-            'questionError' : questionError,
-            'answerError' : answerError,
-            'checkPronunciation' : checkPronunciation
+            'questionText': questionText,
+            'questionIPAText': questionIPAText,
+            'answerText': answerText,
+            'answerIPAText': answerIPAText,
+            'textSimilarity': textSimilarity,
+            'ipaTextSimilarity': ipaTextSimilarity,
+            'scoreComment': scoreComment,
+            'questionError': questionError,
+            'answerError': answerError,
+            'checkPronunciation': checkPronunciation
         }
 
         # 初始化回傳值
         response = {
-            'apiStatus' : 'success',
-            'apiMessage' : 'success',
-            'data' : dataArray,
+            'apiStatus': 'success',
+            'apiMessage': 'success',
+            'data': dataArray,
         }
     except Exception as e:
         # 初始化回傳值
         response = {
-            'apiStatus' : 'error',
-            'apiMessage' : str(e),
+            'apiStatus': 'error',
+            'apiMessage': str(e),
         }
 
     # 將回傳值回傳
     return JsonResponse(response)
-    
+
+
 def checkPronunciation(request):
     try:
         # 蒐集資料
@@ -263,13 +274,12 @@ def checkPronunciation(request):
         if (correctCombo == ''):
             correctCombo = 0
 
-
         # 獲取IPA文字(無標點符號)
         questionIPAText = eng_to_ipa.convert(questionText, False, False)
         answerIPAText = eng_to_ipa.convert(answerText, False, False)
 
         # 處理文字 移除標點符號
-        replaceSymbol= [',', '.', '!', '?']
+        replaceSymbol = [',', '.', '!', '?']
         for i in range(len(replaceSymbol)):
             questionText = questionText.replace(replaceSymbol[i], ' ')
             answerText = answerText.replace(replaceSymbol[i], ' ')
@@ -284,7 +294,8 @@ def checkPronunciation(request):
         ipaTextSimilarity = similar_text(questionIPAText, answerIPAText)
 
         # 根據IPA相似度給予評價
-        scoreComment = appAPIUtils.getScoreComment(ipaTextSimilarity, correctCombo)
+        scoreComment = appAPIUtils.getScoreComment(
+            ipaTextSimilarity, correctCombo)
 
         # 獲取有問題的單字陣列(用發音判斷)
         questionErrorWordArray = []
@@ -311,62 +322,65 @@ def checkPronunciation(request):
         for errorWord in questionErrorWordArray:
             ipaAboutArray = {}
             rhymesWordArray = eng_to_ipa.get_rhymes(errorWord)
-            for ipaAboutWord in sample(rhymesWordArray, min(len(rhymesWordArray),3)):
-                pronunciationData = appAPIUtils.getWordPronunciationData(ipaAboutWord)
+            for ipaAboutWord in sample(rhymesWordArray, min(len(rhymesWordArray), 3)):
+                pronunciationData = appAPIUtils.getWordPronunciationData(
+                    ipaAboutWord)
                 ipaAboutArray.update({ipaAboutWord: pronunciationData})
                 pass
             pronunciationData = appAPIUtils.getWordPronunciationData(errorWord)
-            pronunciationData.update({'ipaAbout' : ipaAboutArray})
+            pronunciationData.update({'ipaAbout': ipaAboutArray})
             questionError.update({errorWord: pronunciationData})
             pass
-        
+
         # 獲取回答中錯誤相關的資料
         answerError = {}
         for errorWord in answerErrorWordArray:
             ipaAboutArray = {}
             rhymesWordArray = eng_to_ipa.get_rhymes(errorWord)
-            for ipaAboutWord in sample(rhymesWordArray, min(len(rhymesWordArray),3)):
-                pronunciationData = appAPIUtils.getWordPronunciationData(ipaAboutWord)
+            for ipaAboutWord in sample(rhymesWordArray, min(len(rhymesWordArray), 3)):
+                pronunciationData = appAPIUtils.getWordPronunciationData(
+                    ipaAboutWord)
                 ipaAboutArray.update({ipaAboutWord: pronunciationData})
                 pass
             pronunciationData = appAPIUtils.getWordPronunciationData(errorWord)
-            pronunciationData.update({'ipaAbout' : ipaAboutArray})
+            pronunciationData.update({'ipaAbout': ipaAboutArray})
             answerError.update({errorWord: pronunciationData})
             pass
 
-
         # IPA發音檢驗
-        checkPronunciation = appAPIUtils.checkPronunciation(questionIPAText, answerIPAText)
+        checkPronunciation = appAPIUtils.checkPronunciation(
+            questionIPAText, answerIPAText)
 
         # 整理資料
         dataArray = {
-            'questionText' : questionText,
-            'questionIPAText' : questionIPAText,
-            'answerText' : answerText,
-            'answerIPAText' : answerIPAText,
-            'textSimilarity' : textSimilarity,
-            'ipaTextSimilarity' : ipaTextSimilarity,
-            'scoreComment' : scoreComment,
-            'questionError' : questionError,
-            'answerError' : answerError,
-            'checkPronunciation' : checkPronunciation,
+            'questionText': questionText,
+            'questionIPAText': questionIPAText,
+            'answerText': answerText,
+            'answerIPAText': answerIPAText,
+            'textSimilarity': textSimilarity,
+            'ipaTextSimilarity': ipaTextSimilarity,
+            'scoreComment': scoreComment,
+            'questionError': questionError,
+            'answerError': answerError,
+            'checkPronunciation': checkPronunciation,
         }
 
         # 初始化回傳值
         response = {
-            'apiStatus' : 'success',
-            'apiMessage' : 'success',
-            'data' : dataArray,
+            'apiStatus': 'success',
+            'apiMessage': 'success',
+            'data': dataArray,
         }
     except Exception as e:
         # 初始化回傳值
         response = {
-            'apiStatus' : 'error',
-            'apiMessage' : str(e),
+            'apiStatus': 'error',
+            'apiMessage': str(e),
         }
 
     # 將回傳值回傳
     return JsonResponse(response)
+
 
 def checkSentences2(request):
     try:
@@ -383,13 +397,12 @@ def checkSentences2(request):
         if (correctCombo == ''):
             correctCombo = 0
 
-
         # 獲取IPA文字(無標點符號)
         questionIPAText = eng_to_ipa.convert(questionText, False, False)
         answerIPAText = eng_to_ipa.convert(answerText, False, False)
 
         # 處理文字 移除標點符號
-        replaceSymbol= [',', '.', '!', '?']
+        replaceSymbol = [',', '.', '!', '?']
         for i in range(len(replaceSymbol)):
             questionText = questionText.replace(replaceSymbol[i], ' ')
             answerText = answerText.replace(replaceSymbol[i], ' ')
@@ -404,40 +417,69 @@ def checkSentences2(request):
         ipaTextSimilarity = similar_text(questionIPAText, answerIPAText)
 
         # 根據IPA相似度給予評價
-        scoreComment = appAPIUtils.getScoreComment(ipaTextSimilarity, correctCombo)
+        scoreComment = appAPIUtils.getScoreComment(
+            ipaTextSimilarity, correctCombo)
 
         # 單字與發音檢驗
         wordCompare = appAPIUtils.wordCompare(questionText, answerText),
 
         # IPA音節檢驗
-        pronunciationCompare = appAPIUtils.textCompare(questionIPAText, answerIPAText)
+        pronunciationCompare = appAPIUtils.textCompare(
+            questionIPAText, answerIPAText)
 
         # 整理資料
         dataArray = {
-            'questionText' : questionText,
-            'questionIPAText' : questionIPAText,
-            'answerText' : answerText,
-            'answerIPAText' : answerIPAText,
-            'textSimilarity' : textSimilarity,
-            'ipaTextSimilarity' : ipaTextSimilarity,
-            'scoreComment' : scoreComment,
-            'wordCompare' : appAPIUtils.wordCompare(questionText, answerText),
-            'pronunciationCompare' : pronunciationCompare, 
+            'questionText': questionText,
+            'questionIPAText': questionIPAText,
+            'answerText': answerText,
+            'answerIPAText': answerIPAText,
+            'textSimilarity': textSimilarity,
+            'ipaTextSimilarity': ipaTextSimilarity,
+            'scoreComment': scoreComment,
+            'wordCompare': appAPIUtils.wordCompare(questionText, answerText),
+            'pronunciationCompare': pronunciationCompare,
         }
 
         # 初始化回傳值
         response = {
-            'apiStatus' : 'success',
-            'apiMessage' : 'success',
-            'data' : dataArray,
+            'apiStatus': 'success',
+            'apiMessage': 'success',
+            'data': dataArray,
         }
     except Exception as e:
         # 初始化回傳值
         response = {
-            'apiStatus' : 'error',
-            'apiMessage' : str(e),
+            'apiStatus': 'error',
+            'apiMessage': str(e),
         }
 
     # 將回傳值回傳
     return JsonResponse(response)
-    
+
+
+def sentSegmentation(request):
+    try:
+        article = request.POST.get('article', '')
+
+        nlp = spacy.load('en_core_web_sm')
+        result_sent = []
+        doc = nlp(article)
+
+        for sent in doc.sents:
+            result = str(sent)
+            result_sent.append(result)
+
+        response = {
+            'apiStatus': 'success',
+            'apiMessage': 'success',
+            'data': result_sent,
+        }
+
+    except Exception as e:
+        # 初始化回傳值
+        response = {
+            'apiStatus': 'error',
+            'apiMessage': str(e),
+        }
+
+    return JsonResponse(response)

e764028ce19cd902753caf5b06abf69186395a4b
m0001123 <65386825+m0001123@users.noreply.github.com>
2022-05-16 16:04:10 +0800

    增加空值及type判斷

--- api/app/code/sentenceController.py
+++ api/app/code/sentenceController.py
@@ -443,9 +443,18 @@ def checkSentences2(request):
 
 def getSentenceIPA(request):
     try:
-        sentenceList = eval (request.POST.get('sentenceList'))
+        sentenceList = request.POST.get('sentenceList')
+        if(sentenceList==""):
+            raise Exception('缺少必填參數 sentenceList')
+        try:
+            sentenceList_eval = eval(sentenceList)
+            if(type(sentenceList_eval)!=type([])):
+                raise Exception("輸入格式必須是List")
+        except Exception as e:
+            raise Exception('輸入格式必須是List')
+        sentenceList_eval = eval (sentenceList)
         ipaList = []
-        for i in sentenceList:
+        for i in sentenceList_eval:
             ipaList.append(eng_to_ipa.convert(i,False,False))
         
         response =  {
